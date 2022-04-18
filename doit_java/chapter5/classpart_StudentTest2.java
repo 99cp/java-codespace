566054a93b55565e6471a98a50450e03143ad724
99cp <103494820+99cp@users.noreply.github.com>
2022-04-18 23:47:20 +0900

    Rename StudentTest2.java to classpart_StudentTest2.java

--- doit_java/chapter5/classpart_StudentTest2.java
+++ doit_java/chapter5/classpart_StudentTest2.java
@@ -4,9 +4,9 @@ public class StudentTest2 {
 
 	public static void main(String[] args) {
 		Student student1 = new Student();
-		student1.studentName = "�ȿ���";
+		student1.studentName = "¾È¿¬¼ö";
 		Student student2 = new Student();
-		student2.studentName = "�Ƚ¿�";
+		student2.studentName = "¾È½Â¿¬";
 		
 		System.out.println(student1);
 		System.out.println(student2);
